ecc7a00b0271404042d175d8bd6bb9afb2b3dae8
kimy87 <paihkimy@gmail.com>
2024-10-15 22:39:26 +0800

    newbranch

---
+++ hello.php
@@ -0,0 +1,7 @@
+<?php
+echo"<center>";
+echo"<h1>Welcome to new branch 20241015</h1>";
+echo"<p><a href='index.php'>Back</a></p>";
+echo"</center>";
+
+?>
--- index.php
+++ index.php
@@ -1,4 +1,7 @@
 <?php
-echo"<center><h1>Welcome to new github test</h1></center>";
+echo"<center>";
+echo"<h1>Welcome to new github test</h1>";
+echo"<p><a href='hello.php'>This is Branch version 20241015</a></p>";
+echo"</center>";
 
 ?>
