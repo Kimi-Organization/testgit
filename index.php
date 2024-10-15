73425957244eabe8a8562c0a43f992757243c3e0
kimy87 <paihkimy@gmail.com>
2024-10-15 22:45:30 +0800

    Update index.php

--- index.php
+++ index.php
@@ -2,8 +2,8 @@
 echo"<center>";
 echo"<h1>Welcome to new github test</h1>";
 echo"<p><a href='hello.php'>This is Branch version 20241015</a></p>";
-echo"</center>";
-echo"<center><h1>Welcome to new github test</h1></center>";
 echo"developed by test";
+echo"</center>";
+
 
 ?>
