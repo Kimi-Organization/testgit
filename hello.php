9da416ad8593fee0874c9fbcd815978c2f575df6
kimy87 <paihkimy@gmail.com>
2024-12-03 10:30:16 +0800

    Revert "Merge branch 'main' into branch_20241015-v1"
    This reverts commit 267fab76784d2badfce397fad60d0e736b9a5a08, reversing
    changes made to 0d1521dd3bf5071c3da74272dabef2be63636c97.

--- hello.php
+++ hello.php
@@ -1,6 +1,6 @@
 <?php
 echo"<center>";
-echo"<h1>Welcome to new branch 20241015_v1</h1>";
+echo"<h1>Welcome to new branch 20241015</h1>";
 echo"<p><a href='index.php'>Back</a></p>";
 echo"</center>";
 
